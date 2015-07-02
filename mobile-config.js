cb0d9886710416dd4902b8deddae54e1af46a389
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2015-07-01 21:14:47 -0700

    Add icons, mobile config

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+App.info({
+  name: 'SampleMe',
+  description: 'Stochastic self-tracking on web and mobile',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
