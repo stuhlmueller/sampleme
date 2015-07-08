9f5eef97c6d58e438566701e8bfba0a343d0120e
Noah Goodman <ngoodman@stanford.edu>
2015-07-08 09:02:09 -0700

    forms and such

--- client/ndg.js
+++ client/ndg.js
@@ -11,19 +11,45 @@ Template.trackerTree.helpers(
   {
     branches: function(){return trackerOntology.branches},
     addFullnames: function(bs){
-      bs.forEach(function(b){b.fullname=trackerOntology.name+":"+b.name})
+      bs.forEach(function(b){b.fullname=trackerOntology.name+"-"+b.name})
     }
   })
 
-  Template.branch.helpers(
+Template.branch.helpers(
     {
       extendBranchFullNames: function(bs){
         var fullname = this.fullname
-        bs.forEach(function(b){b.fullname=fullname+":"+b.name})
-      },
-      extendInputFullNames: function(is){
-        var fullname = this.fullname
-        is.forEach(function(i){i.fullname=fullname+":"+i.name})
+        bs.forEach(function(b){b.fullname=fullname+"-"+b.name})
       }
     }
   )
+
+Template.inputs.helpers(
+      {
+        extendInputFullNames: function(is){
+          var fullname = this.fullname
+          is.forEach(function(i){i.fullname=fullname+"-"+i.name})
+        }
+      }
+    )
+
+Template.inputs.events({
+      'submit form': function(e,t) {
+        e.preventDefault();
+
+        //get info from all the input elements
+        var data = t.$(':input').map(function(ind,obj){
+          return {name: obj.name, value: obj.value}}) //checkme: value or val()?
+
+        console.log(data)
+
+        // Meteor.call('insertEvent', rating, function(error, results){
+        //   if (error){
+        //     return alert(error.reason);
+        //   } else {
+        //     // reset slider
+        //     $("#rating").slider();
+        //   }
+        // });
+      }
+    });
