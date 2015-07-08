7cb222d9da896f5877d27ef015c29906cc294972
Noah Goodman <ngoodman@stanford.edu>
2015-07-07 20:52:05 -0700

    construct fullnames to make submission more straightforward

--- client/ndg.js
+++ client/ndg.js
@@ -1,12 +1,29 @@
 var trackerOntology =
-  [{name: "Eat", inputs: [{name: "what", type: "text"}] },
+{name: "base",
+ branches: [{name: "Eat", inputs: [{name: "what", type: "text"}] },
    {name: "Drink", branches:
                     [{name: "Caffeine", inputs: [{name: "quantity", type: "slider"}]},
                      {name: "Alchohol", inputs: [{name: "quantity", type: "slider"}]}]}
   ]
+}
 
 Template.trackerTree.helpers(
-  {branches: function() {
-    return trackerOntology;
+  {
+    branches: function(){return trackerOntology.branches},
+    addFullnames: function(bs){
+      bs.forEach(function(b){b.fullname=trackerOntology.name+":"+b.name})
     }
   })
+
+  Template.branch.helpers(
+    {
+      extendBranchFullNames: function(bs){
+        var fullname = this.fullname
+        bs.forEach(function(b){b.fullname=fullname+":"+b.name})
+      },
+      extendInputFullNames: function(is){
+        var fullname = this.fullname
+        is.forEach(function(i){i.fullname=fullname+":"+i.name})
+      }
+    }
+  )
