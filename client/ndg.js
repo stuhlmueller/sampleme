898fc6804aa62a86d1d6bbc87faf1ae86865fcc1
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2015-07-08 17:27:37 -0700

    Move fullName computation out of template

--- client/ndg.js
+++ client/ndg.js
@@ -1,35 +1,39 @@
-var trackerOntology = {
-  name: "base",
-  branches: [{name: "Eat", inputs: [{name: "what", type: "text"}] },
-             {name: "Drink", branches:
-              [{name: "Caffeine", inputs: [{name: "quantity", type: "slider"}]},
-               {name: "Alchohol", inputs: [{name: "quantity", type: "slider"}]}]}
-            ]
+var addFullNames = function(ontology){
+  if (!ontology.fullName){
+    ontology.fullName = ontology.name; // root
+  }
+  if (ontology.branches){
+    ontology.branches.forEach(function(branch){
+      branch.fullName = ontology.fullName + '-' + branch.name;
+      addFullNames(branch);
+    });
+  }
+  if (ontology.inputs){
+    ontology.inputs.forEach(function(input){
+      input.fullName = ontology.fullName + '-' + input.name;
+    });
+  }
 };
 
+var trackerOntology = {
+  name: "Base",
+  branches: [
+    {name: "Eat", inputs: [{name: "What", type: "text"}] },
+    {name: "Drink", branches:
+     [{name: "Caffeine", inputs: [{name: "Quantity", type: "slider"}]},
+      {name: "Alchohol", inputs: [{name: "Quantity", type: "slider"}]}]}]
+};
+
+addFullNames(trackerOntology);
+
+
 Template.trackerTree.helpers({
-  branches: function(){return trackerOntology.branches},
-  addFullnames: function(bs){
-    bs.forEach(function(b){b.fullname=trackerOntology.name+"-"+b.name})
-  }
+  branches: function(){return trackerOntology.branches}
 });
 
-Template.branch.helpers({
-  extendBranchFullNames: function(bs){
-    var fullname = this.fullname
-    bs.forEach(function(b){b.fullname=fullname+"-"+b.name})
-  }
-});
-
-Template.inputs.helpers({
-  extendInputFullNames: function(is){
-    var fullname = this.fullname
-    is.forEach(function(i){i.fullname=fullname+"-"+i.name})
-  }
-});
 
 Template.inputs.events({
-  'submit form': function(e,t) {
+  'submit form': function(e, t) {
     e.preventDefault();
 
     //get info from all the input elements
